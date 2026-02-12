1de2107c384b6a45cd4aa38dad33d34f288a319c
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2026-02-12 21:53:06 +0100

    scan identifiers and keywords

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,3 +1,23 @@
+// sorted for binary search
+const KEYWORDS: &[(&str, Token)] = &[
+    ("break", Token::Break),
+    ("const", Token::Const),
+    ("continue", Token::Continue),
+    ("else", Token::Else),
+    ("fn", Token::Fn),
+    ("goto", Token::Goto),
+    ("i32", Token::I32),
+    ("if", Token::If),
+    ("ptr", Token::Ptr),
+    ("return", Token::Return),
+    ("struct", Token::Struct),
+    ("syscall", Token::Syscall),
+    ("u32", Token::U32),
+    ("u8", Token::U8),
+    ("var", Token::Var),
+    ("while", Token::While),
+];
+
 pub struct Lexer<'a> {
     source: &'a [u8],
     position: usize,
@@ -38,6 +58,28 @@ impl<'a> Lexer<'a> {
     fn scan_token(&self) -> Result<(Token, usize), LexError> {
         match self.current {
             Some(b'*') => Ok((Token::Star, 1)),
+            Some(ch) if ch.is_ascii_alphabetic() || ch == b'_' => {
+                // scan identifier or keyword
+                let mut len = 0;
+                while let Some(c) = self.source.get(self.position + len) {
+                    if c.is_ascii_alphanumeric() || *c == b'_' {
+                        len += 1;
+                    } else {
+                        break;
+                    }
+                }
+
+                let text = std::str::from_utf8(&self.source[self.position..self.position + len])
+                    .expect("identifiers are ascii");
+
+                // binary search in KEYWORDS
+                let token = match KEYWORDS.binary_search_by_key(&text, |&(s, _)| s) {
+                    Ok(idx) => KEYWORDS[idx].1.clone(),
+                    Err(_) => Token::Identifier(text.to_string()),
+                };
+
+                Ok((token, len))
+            }
             _ => todo!(),
         }
     }
