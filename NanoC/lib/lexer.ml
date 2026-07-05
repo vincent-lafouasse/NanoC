5771c9d464b07e59e659927456f65b59b68ffe11
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2026-07-06 00:03:38 +0200

    make Parser.t constructor

--- NanoC/lib/lexer.ml
+++ NanoC/lib/lexer.ml
@@ -9,6 +9,11 @@ type error =
   | UnrecognizedCharacter of char
 [@@deriving show]
 
+let init (input : string) : t =
+  let position : Position.t = { absolute = 0; line = 1; column = 0 } in
+  { input; position }
+;;
+
 let tokenize () = ()
 
 let say_hi () = "hi"
