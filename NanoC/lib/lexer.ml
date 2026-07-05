aabe09bcea0952e7a51c94bc2b7a2f5c8c8e0926
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2026-07-06 00:02:22 +0200

    init Lexer.t and Lexer.error

--- NanoC/lib/lexer.ml
+++ NanoC/lib/lexer.ml
@@ -1,3 +1,14 @@
+type t =
+  { input : string
+  ; position : Position.t
+  }
+[@@deriving show]
+
+type error =
+  | UnterminatedString
+  | UnrecognizedCharacter of char
+[@@deriving show]
+
 let tokenize () = ()
 
 let say_hi () = "hi"
