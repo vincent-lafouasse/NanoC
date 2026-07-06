43a2c3fbe6656123355fd8220f6e9388220de719
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2026-07-06 20:01:48 +0200

    refactor

--- NanoC/bin/main.ml
+++ NanoC/bin/main.ml
@@ -5,15 +5,17 @@ let show_char_option = function
   | None -> "None"
 ;;
 
+let show_lexer_char l = show_char_option (Lexer.get l)
+
 let () =
   let lexer : Lexer.t = Lexer.init "   a   b  \n c   d" in
-  let _ = print_endline (show_char_option (Lexer.get lexer)) in
+  let _ = print_endline (show_lexer_char lexer) in
   let lexer = Lexer.skip_whitespace lexer in
-  let _ = print_endline (show_char_option (Lexer.get lexer)) in
+  let _ = print_endline (show_lexer_char lexer) in
   let lexer = Lexer.skip_whitespace lexer in
-  let _ = print_endline (show_char_option (Lexer.get lexer)) in
+  let _ = print_endline (show_lexer_char lexer) in
   let lexer = Lexer.skip_whitespace lexer in
-  let _ = print_endline (show_char_option (Lexer.get lexer)) in
+  let _ = print_endline (show_lexer_char lexer) in
   let lexer = Lexer.skip_whitespace lexer in
-  print_endline (show_char_option (Lexer.get lexer))
+  print_endline (show_lexer_char lexer)
 ;;
