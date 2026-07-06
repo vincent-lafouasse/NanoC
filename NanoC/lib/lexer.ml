bb8d40ec73dc4fb8a4e3396d9f6b48513627f852
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2026-07-06 19:12:15 +0200

    remove dummy code

--- NanoC/lib/lexer.ml
+++ NanoC/lib/lexer.ml
@@ -13,9 +13,3 @@ let init (input : string) : t =
   let position : Position.t = { absolute = 0; line = 1; column = 0 } in
   { input; position }
 ;;
-
-let tokenize () = ()
-
-let say_hi () = "hi"
-
-let%test "say_hi returns hi" = say_hi () = "hi"
