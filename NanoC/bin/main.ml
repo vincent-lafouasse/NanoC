94c50e90634923b575b513ff8b3812e829bf63e4
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2026-07-06 20:02:10 +0200

    minor refactor

--- NanoC/bin/main.ml
+++ NanoC/bin/main.ml
@@ -8,7 +8,7 @@ let show_char_option = function
 let show_lexer_char l = show_char_option (Lexer.get l)
 
 let () =
-  let lexer : Lexer.t = Lexer.init "   a   b  \n c   d" in
+  let lexer : Lexer.t = Lexer.init " \t a   b  \n c   d" in
   let _ = print_endline (show_lexer_char lexer) in
   let lexer = Lexer.skip_whitespace lexer in
   let _ = print_endline (show_lexer_char lexer) in
