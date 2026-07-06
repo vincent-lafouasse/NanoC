f696f654937b086138d07cab4e6d77d9a29cef7d
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2026-07-06 19:12:28 +0200

    format

--- NanoC/bin/main.ml
+++ NanoC/bin/main.ml
@@ -1,5 +1,6 @@
 open NanoC
 
 let () =
-    let lexer : Lexer.t = Lexer.init "abc" in
-    print_endline (Lexer.show lexer)
+  let lexer : Lexer.t = Lexer.init "abc" in
+  print_endline (Lexer.show lexer)
+;;
