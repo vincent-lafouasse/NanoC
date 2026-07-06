d29581b183e665f264ce4f1e5c02f2a2f5e357b0
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2026-07-06 20:00:30 +0200

    lgos

--- NanoC/bin/main.ml
+++ NanoC/bin/main.ml
@@ -1,14 +1,18 @@
 open NanoC
 
+let show_char_option = function
+  | Some c -> Printf.sprintf "Some '%c'" c
+  | None -> "None"
+
 let () =
-  let lexer : Lexer.t = Lexer.init "a\nc" in
-  let _ = print_endline (Lexer.show lexer) in
-  let lexer = Lexer.advance lexer in
-  let _ = print_endline (Lexer.show lexer) in
-  let lexer = Lexer.advance lexer in
-  let _ = print_endline (Lexer.show lexer) in
-  let lexer = Lexer.advance lexer in
-  let _ = print_endline (Lexer.show lexer) in
-  let lexer = Lexer.advance lexer in
-  print_endline (Lexer.show lexer)
+  let lexer : Lexer.t = Lexer.init "   a   b  \n c   d" in
+  let _ = print_endline (show_char_option (Lexer.get lexer)) in
+  let lexer = Lexer.skip_whitespace lexer in
+  let _ = print_endline (show_char_option (Lexer.get lexer)) in
+  let lexer = Lexer.skip_whitespace lexer in
+  let _ = print_endline (show_char_option (Lexer.get lexer)) in
+  let lexer = Lexer.skip_whitespace lexer in
+  let _ = print_endline (show_char_option (Lexer.get lexer)) in
+  let lexer = Lexer.skip_whitespace lexer in
+  print_endline (show_char_option (Lexer.get lexer))
 ;;
