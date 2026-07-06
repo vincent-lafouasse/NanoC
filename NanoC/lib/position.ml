7262d9995577aa81265b8eafd96d09be6c6814f9
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2026-07-06 19:45:21 +0200

    move position advance logic to Position.advance and Position.newline

--- NanoC/lib/position.ml
+++ NanoC/lib/position.ml
@@ -8,3 +8,6 @@ type t =
 type 'a located = 'a * t [@@deriving show]
 
 let distance pos_from pos_to = pos_to.absolute - pos_from.absolute
+
+let newline pos = { absolute = pos.absolute + 1; line = pos.line + 1; column = 0 }
+let advance pos = { pos with absolute = pos.absolute + 1; column = pos.column + 1 }
