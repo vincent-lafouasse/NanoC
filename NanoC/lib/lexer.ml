43342bd2dc27523ef3e48afcb70085a1d93f714c
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2026-07-06 19:36:32 +0200

    sketch

--- NanoC/lib/lexer.ml
+++ NanoC/lib/lexer.ml
@@ -18,8 +18,16 @@ let len lexer = String.length lexer.input
 
 let eof lexer = lexer.position.absolute >= len lexer
 
-let at lexer (position: Position.t) = String.get lexer.input position.absolute
+let at lexer (position : Position.t) = String.get lexer.input position.absolute
 
 let get_unsafe lexer = at lexer lexer.position
 
 let get lexer : char option = if eof lexer then None else Some (get_unsafe lexer)
+
+let advance lexer : t =
+  let pos_advance_naive (pos : Position.t) : Position.t =
+    let absolute = pos.absolute + 1 in
+    { pos with absolute }
+  in
+  failwith "todo"
+;;
