06128728c6316c17005bf5316d319fd8b52de316
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2026-07-08 16:50:28 +0200

    log tokens by iterating

--- NanoC/bin/main.ml
+++ NanoC/bin/main.ml
@@ -12,9 +12,14 @@ let tokenize_or_die source =
   | Error e -> die (Lexer.format_error e)
 ;;
 
+let log_tokens tokens =
+  let log_single_token tok = print_endline (Token.show tok) in
+  Array.iter log_single_token tokens
+;;
+
 let () =
   let source = "fn undefined zeroed struct u8 aaa bbb _420" in
   let () = print_endline ("source:\n" ^ source ^ "\n") in
-  let _tokens = tokenize_or_die source in
-  ()
+  let tokens = tokenize_or_die source in
+  log_tokens tokens
 ;;
