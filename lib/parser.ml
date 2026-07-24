4ea46b25cfb4952b1b8e882646982f064ade7faf
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2026-07-24 12:25:15 +0200

    implement Precedence.to_int

--- lib/parser.ml
+++ lib/parser.ml
@@ -17,6 +17,23 @@ module Precedence = struct
     | Factor (* * / % *)
     | Prefix (* ! ~ - & *)
     | Postfix (* -> . [] () *)
+
+  let to_int = function
+    | None -> 0
+    | Assignment -> 1
+    | LogicalOr -> 2
+    | LogicalAnd -> 3
+    | BitwiseOr -> 4
+    | BitwiseXor -> 5
+    | BitwiseAnd -> 6
+    | Equality -> 7
+    | Comparison -> 8
+    | Shift -> 9
+    | Term -> 10
+    | Factor -> 11
+    | Prefix -> 12
+    | Postfix -> 13
+  ;;
 end
 
 type literal = IntLiteral of int64
