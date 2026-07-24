6190710a4ec75c0b7a50ee0fb802ba08ae272dc4
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2026-07-24 17:57:17 +0200

    structure expr enum using inline structs

--- lib/ast.ml
+++ lib/ast.ml
@@ -36,12 +36,12 @@ type literal = IntLiteral of int64 [@@deriving show]
 type expr =
   | Literal of literal
   | Identifier of string
-  | Binary of BinaryOp.t * expr * expr
-  | Unary of UnaryOp.t * expr
+  | Binary of { op : BinaryOp.t; lhs : expr; rhs : expr }
+  | Unary of { op : UnaryOp.t; operand : expr }
   | Syscall of string list
   | Grouping of expr
-  | Call of expr * expr list
-  | DotAccess of expr * string
-  | ArrowAccess of expr * string
-  | Index of expr * expr
+  | Call of { callee : expr; args : expr list }
+  | DotAccess of { target : expr; field : string }
+  | ArrowAccess of { target : expr; field : string }
+  | Index of { target : expr; index : expr }
 [@@deriving show]
