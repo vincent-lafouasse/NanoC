1bec48665a5da4df6f9091fcf8d339901c2afdef
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2026-07-24 12:23:20 +0200

    introduce Precedence module for Pratt parsing

--- lib/parser.ml
+++ lib/parser.ml
@@ -1,3 +1,24 @@
+module Precedence = struct
+  (* higher = tighter binding
+     per https://en.cppreference.com/w/c/language/operator_precedence.html
+  *)
+  type t =
+    | None
+    | Assignment (* = *)
+    | LogicalOr (*  || *)
+    | LogicalAnd (* && *)
+    | BitwiseOr (* | *)
+    | BitwiseXor (* ^ *)
+    | BitwiseAnd (* & *)
+    | Equality (* == != *)
+    | Comparison (* < > <= >= *)
+    | Shift (* << >> *)
+    | Term (* + - *)
+    | Factor (* * / % *)
+    | Prefix (* ! ~ - & *)
+    | Postfix (* -> . [] () *)
+end
+
 type literal = IntLiteral of int64
 
 type expression = Literal of literal
